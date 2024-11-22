b682409c8888b126c96dce114b05324c0552e3ca
aman9483 <amankumarverma251@gmail.com>
2024-11-22 14:10:22 +0530

    correct the error

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'; 
 import './index.css';
 import SignIn from './pages/SignIn';
-import Navbar from './components/Navbar/navbar';
+import Navbar from './components/Navbar/navbar.jsx';
 import Home from './pages/Home';
-
 import Menu from './pages/Menu';
-import Cart from './pages/Cart';
-import Error from './pages/error'; // Import your Error page component
+
+import Error from './pages/error';
 
 function App() {
   return (
@@ -17,8 +16,6 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/sign-in" element={<SignIn />} />
         <Route exact path="/coffee" element={<Menu />} />
-     
-        
     
         <Route path="*" element={<Error />} />
       </Routes>
